12de13c75d8d847db008b6a144acf84ed2983666
Himanshu-gupta31 <himanshu.gupta0031@gmail.com>
2024-07-13 15:27:00 +0530

    fixed card conflict

--- src/Component/Card.tsx
+++ src/Component/Card.tsx
@@ -1,7 +1,8 @@
+
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 interface CardProps{
-    image:string,
+    image:string
     name:string,
     title:string,
     description:string,
@@ -20,15 +21,15 @@ export const Card:React.FC<CardProps>=({
 })=>{
     return(
        <>
-       <div className='w-[20rem] h-[33rem] bg-white border border-gray-400 flex flex-col '>
+       <div className='w-[20rem] h-[25rem] bg-white border border-gray-400 flex flex-col '>
              <div className='h-1/2 bg-gray-800'>
-            <img src={image} className='h-full w-full object-cover'></img>
+            {image}
              </div>
              <div className='p-2 pl-4'>
              <h1 className='font-semibold mt-4 text-xl'>{name}</h1>
              <h2 className='font-normal text-gray-600 mt-4 text-md'>{title}</h2>
              <p className='mt-4 text-gray-400'>{description}</p>
-             <p className='mt-4 text-black'>{experience}</p>
+             <p className='text-black font-semibold'>{experience}</p>
              <div className='bg-gray-200 rounded-full w-[2rem] h-[2rem] flex justify-center items-center mt-4'>
              {socialLink ? (
               <a href={socialLink}>
